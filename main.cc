82dc325ca234ca03f0ae11419216ded767bfb75b
Dark Wang <dkfire30@outlook.com>
2020-03-24 09:41:57 +0800

    Depth-first Traversal (recursive)
    Resolve #9
    
    Signed-off-by: Dark Wang <dkfire30@outlook.com>

--- main.cc
+++ main.cc
@@ -9,30 +9,23 @@
 // #include "LinkedList2.h"
 // #include "Stack.h"
 // #include "BracketMatching.h"
-#include "Queue.h"
+// #include "Queue.h"
+#include "BinaryTree.h"
 
 // void foo(int t) { std::cout << t << std::endl; }
 
-// test for pop & push & isEmpty & size
+// test for creat and traversal
 TEST(Suite1, Test1) {
-  Queue<std::string> q;
-  const size_t cap = 10;
-  int n = cap;
-  while (n) {
-    q.push(std::to_string(n));
-    --n;
-  }
-  EXPECT_EQ(cap, q.size());
-  EXPECT_EQ(0, n);
+  BinaryTree b;
+  std::vector<int> v{1, 2, 3, 0, 0, 4, 5, 0, 0, 0, 6, 0, 0};
+  BinaryTree::pointer_to_node root = b.root();
+  b.create(root, v);
 
-  n = cap;
-  while (!q.isEmpty()) {
-    EXPECT_EQ(std::to_string(n), q.top());
-    q.pop();
-    --n;
-  }
-  EXPECT_EQ(0, q.size());
-  EXPECT_EQ(0, n);
+  b.pre_traversal(root, [](int a) { std::cout << a << std::endl; });
+  std::cout << "---" << std::endl;
+  b.mid_traversal(root, [](int a) { std::cout << a << std::endl; });
+  std::cout << "---" << std::endl;
+  b.post_traversal(root, [](int a) { std::cout << a << std::endl; });
 }
 
 int main(int argc, char **argv) {
